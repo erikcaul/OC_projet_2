d898678b0f7fa62c558009f2869cddfa5a28371d
erikcaul <erikcaul@gmail.com>
2021-08-31 10:50:03 -0400

    Revue du code pour générer des url sans ".." (2nd url - url page produit)

--- extract_books_url_list_per_category.py
+++ extract_books_url_list_per_category.py
@@ -19,9 +19,8 @@ def extract_books_url_list_per_category(url):
         for h3 in h3s:
             a = h3.find('a')
             product_page_url = a['href']
-            oldurl = url
-            path = oldurl.rsplit('/', 1)[0]
-            product_page_url = path + '/' + product_page_url
+            url_split = product_page_url.rsplit('/', 2)[1:3]
+            product_page_url = 'https://books.toscrape.com/catalogue/' + url_split[0] + '/' + url_split[1]
             data_books_category_list.append(extract_transf_books_data(product_page_url))
             image_url = extract_img_url(product_page_url)
             category = extract_category(product_page_url)
