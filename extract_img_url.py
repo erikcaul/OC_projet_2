98e0a03837c35dded276404850ed427a5edf0bd1
erikcaul <erikcaul@gmail.com>
2021-08-31 09:51:05 -0400

    Revue du code pour générer des url sans ".."

--- extract_img_url.py
+++ extract_img_url.py
@@ -9,6 +9,8 @@ def extract_img_url(url):
         active_image = soup.find(class_='item')
         image = active_image.find('img')
         image_url = image['src']
-        path = url.rsplit('/', 1)[0]
-        image_url = path + '/' + image_url
+        url_split = image_url.rsplit('/', 5)[1:6]
+        image_url = 'http://books.toscrape.com'
+        for i in url_split:
+            image_url += '/' + i
         return image_url
--- script.py
+++ script.py
@@ -2,7 +2,6 @@ from extract_category_url_list import extract_category_url_list
 from extract_books_url_list_per_category import extract_books_url_list_per_category
 from save_data import data_saving
 
-
 category_url_list = extract_category_url_list('http://books.toscrape.com/index.html')
 for category_url in category_url_list:
     url_list_per_category = extract_books_url_list_per_category(category_url)
